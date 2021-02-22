05368c1f43719a512f80e9cd19eeac3712621675
Patrick <patrick.on.the.web@gmail.com>
2021-02-22 18:46:03 -0500

    Consolidating user lookup to SecurityUtils

--- src/main/java/com/patrickontheweb/fit/configuration/AuditorAwareImpl.java
+++ src/main/java/com/patrickontheweb/fit/configuration/AuditorAwareImpl.java
@@ -3,20 +3,16 @@ package com.patrickontheweb.fit.configuration;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.patrickontheweb.fit.model.user.User;
-import com.patrickontheweb.fit.security.AppUserDetails;
+import com.patrickontheweb.fit.security.SecurityUtils;
 
 @Component
 public class AuditorAwareImpl implements AuditorAware<User> {
 	 
     @Override
     public Optional<User> getCurrentAuditor() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        AppUserDetails userDetails = AppUserDetails.class.cast(authentication.getPrincipal());
-        return Optional.ofNullable(userDetails.getUser());
+        return SecurityUtils.getCurrentUser();
     }
 }
